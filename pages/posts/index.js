b19a80d202b4ebc84415160868d7657c436c7602
GSuleh <gerald.suleh@strathmore.edu>
2023-03-12 21:39:36 +0300

    subscribe functionality- add check.

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -2,8 +2,44 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { getAllPosts, getAuthorBySlug  } from '../../lib/api'
 import { Card } from "flowbite-react"
+import { useRouter } from "next/router"
+import { useEffect, useState } from 'react';
+import { WsProvider, ApiPromise } from '@polkadot/api';
+
 
 export default function Posts({ posts }) {
+
+  const [extensionDapp, setExtensionDapp] = useState(null);
+  useEffect(() => {
+      import('@polkadot/extension-dapp')
+        .then((module) => {
+          setExtensionDapp(module);
+        })
+        .catch((error) => {
+          console.error('Error loading @polkadot/extension-dapp:', error);
+        });
+    }, []);
+
+  const wsProvider = new WsProvider('wss://rococo-contracts-rpc.polkadot.io');
+  const api = ApiPromise.create({ provider: wsProvider });
+
+  const router = useRouter();
+
+  const navigation = async(api,post) => {
+    const  p = await api;
+    const extensions = await extensionDapp.web3Enable('payblog');
+    const accounts = await extensionDapp.web3Accounts();
+    const account = accounts[0];
+    const { nonce } = await p.query.system.account(account.address);
+  
+    if (nonce.gt(BigInt(0))) {
+      router.push(post.permalink);
+    } else {
+      // Display modal or alert
+      alert('You must be subscribed to access this post.');
+    }
+  }
+
    return (
     <div className="mb-3">
       <h1 className="mb-3">Posts</h1>
@@ -18,14 +54,14 @@ export default function Posts({ posts }) {
 
         return (
           <Card
-        className="mb-3 mt-3"
+          className="mb-3 mt-3"
         >
           <article key={post.slug}>
-              <Link href={post.permalink}>
+          <Link href="#" onClick={() => navigation(api,post)}>
               <h3 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-3">
                 {post.title}
               </h3>
-              </Link>
+          </Link>
 
             <time dateTime={post.createdAt}>{prettyDate}</time>
 
@@ -38,7 +74,7 @@ export default function Posts({ posts }) {
 
             <h5 className="font-normal text-gray-700 dark:text-gray-400">{post.excerpt}</h5>
 
-            <Link href={post.permalink}>
+            <Link href="#" onClick={() => navigation(api,post)}>
               Read more →
             </Link>
           </article>
